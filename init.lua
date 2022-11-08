a120e67ef7286adc46336072a11c900392b88760
kraxli <kraxli@pm.me>
2022-11-08 22:02:35 +0100

    fix formatting for lua files

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ IS_WINDOWS = (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1)
 
 -- add a directory to runtimerpath:
 -- vim.opt.rtp:append("~/.config/astronvim/lua/user")
-
 pcall(require, "local.settings")
 
 return {}
--- lsp/formatting.lua
+++ lsp/formatting.lua
@@ -1,3 +1,10 @@
 return {
-  disabled = { "sumneko_lua" },
+	disabled = { "sumneko_lua" }, -- disable languager servers / linters
+	format_on_save = {
+		-- enabled = false, -- enable format on save
+		ignore_filetypes = { -- disable format on save for specified filetypes
+			"lua",
+		},
+		-- allow_filetypes = { }, -- only allow formatting on save for these filetypes
+	},
 }
