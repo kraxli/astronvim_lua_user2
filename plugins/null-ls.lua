8c72128114e63122f74558ea42768f8f614bc882
kraxli <kraxli@pm.me>
2022-11-08 22:06:03 +0100

    null-ls formatting

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -23,7 +23,7 @@ return function(config)
 		null_ls.builtins.diagnostics.yamllint,
 		refurb,
 		-- Set a formatter
-		formatting.stylua,
+		-- formatting.stylua,
 		formatting.black,
 		formatting.prettierd.with({
 			env = {
@@ -37,14 +37,17 @@ return function(config)
 	config.on_attach = function(client)
 		if client.server_capabilities.documentFormattingProvider then
 			vim.api.nvim_create_augroup("format_on_save", { clear = true })
-			-- vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 	desc = "Auto format before save",
-			-- 	group = "format_on_save",
-			-- 	pattern = "<buffer>",
-			-- 	callback = function()
-			-- 		-- vim.lsp.buf.formatting_sync({ async = true })
-			-- 	end,
-			-- })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				desc = "Auto format before save",
+				group = "format_on_save",
+				pattern = "<buffer>",
+				callback = function()
+					-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+					-- vim.lsp.buf.format({ async = false })
+				end,
+			})
 		end
 	end
 	return config
