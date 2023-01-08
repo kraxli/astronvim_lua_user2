0e9c377c2105f1b706e63e96498f9232b996bd36
kraxli <kraxli@pm.me>
2023-01-08 10:56:51 +0100

    clean autocmd / auto commands

--- autocmds.lua
+++ autocmds.lua
@@ -12,44 +12,6 @@ vim.api.nvim_create_autocmd("VimLeave", {
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
--- vim.api.nvim_create_augroup("TextYankPost", { clear = true })
--- vim.api.nvim_create_autocmd("TextYankPost", {
--- 	desc = "Highlight text while yanking",
--- 	group = "TextYankPost",
--- 	pattern = "*",
--- 	callback = function()
--- 		require("vim.highlight").on_yank({ higroup = "IncSearch", timeout = 150 })
--- 	end, -- higroup = 'Visual'
--- })
--- -- autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup="IncSearch", timeout=150})   -- higroup = 'Visual'
-
--- augroup("_terminal", {})
--- autocmd("TermEnter term://*toggleterm#*",{
---   desc="ToggleTerm with <c-t> in terminal mode",
---   group='_terminal',
---   command='tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>',
--- })
-
--- augroup("FloatingWindow", { clear = true })
--- autocmd("TermEnter term://*toggleterm#*", {
--- 	desc = "Close floating window",
--- 	group = "FloatingWindow",
--- 	-- pattern = "*",
--- 	command = "nnoremap <silent><buffer> q :close<CR>",
--- 	-- command = "noremap q <c-\\><c-n>:q<cr>" ,
--- 	-- command='tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>',
--- })
-
--- augroup("MarkdownBuf", { clear = true })
--- autocmd({ "BufRead", "BufEnter", "BufWinEnter", "BufNew", "VimEnter", "InsertEnter" }, {
--- 	group = "MarkdownBuf",
--- 	desc = "Buffer markdown",
--- 	pattern = { "*.md", "*.wiki" },
--- 	callback = function()
--- 		require("user.highlight").markdown()
--- 	end,
--- 	-- command = "lua require('user.highlight').markdown()",
--- })
 
 vim.cmd([[
 
@@ -86,13 +48,4 @@ vim.cmd([[
   augroup end
 
 ]])
-
--- vim.api.nvim_create_augroup("mini", { clear = true })
--- vim.api.nvim_create_autocmd({ "BufEnter", "TermOpen" }, {
--- 	desc = "Disable indent scope for conent types",
--- 	group = "mini",
--- 	callback = function()
--- 		vim.b.miniindentscope_disable = vim.tbl_contains({ "help", "terminal", "nofile", "prompt" }, vim.bo.buftype)
--- 	end,
--- })
 
