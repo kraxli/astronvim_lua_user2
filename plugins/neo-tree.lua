eb8703d32ed3b63f2f8b93b50ddfc2a86f05247f
kraxli <kraxli@pm.me>
2023-03-10 23:03:48 +0100

    neotree experiment

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,3 +1,10 @@
-return{
-  follow_current_file = false,
+return {
+--   follow_current_file = false,
+  filesystem = {
+    follow_current_file = false, --true,
+    hijack_netrw_behavior = "disabled", -- "open_current",
+  },
+  buffers = {
+    follow_current_file = false, --true,
+  },
 }
