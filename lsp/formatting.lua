c7ab8e33b21f23f68fbc7a76153a3b9562bce25c
kraxli <kraxli@pm.me>
2023-03-21 06:30:16 +0100

    lsp formatting lua

--- lsp/formatting.lua
+++ lsp/formatting.lua
@@ -1,1 +1,10 @@
-return { disabled = { "lua_ls" } }
+return {
+	disabled = { "lua_ls", "sumneko_lua" },
+	format_on_save = {
+		-- enabled = false, -- enable format on save
+		ignore_filetypes = { -- disable format on save for specified filetypes
+			"lua",
+		},
+		-- allow_filetypes = { }, -- only allow formatting on save for these filetypes
+	},
+}
