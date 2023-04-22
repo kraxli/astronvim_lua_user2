3e0116d4c421f3edb7a53e15f76766deb1d35698
kraxli <kraxli@pm.me>
2023-04-22 14:49:43 +0200

    writer plusings config

--- plugins/writer.lua
+++ plugins/writer.lua
@@ -30,5 +30,47 @@ return {
     	autolist.create_mapping_hook("n", "<C-r>", autolist.force_recalculate)
     	autolist.create_mapping_hook("n", "<leader>x", autolist.invert_entry, "")
   	end,
+		enabled = false,
+	},
+	{
+		"dkarter/bullets.vim",
+		ft = {
+    	"markdown",
+    	"text",
+    	"tex",
+    	"plaintex",
+  	},
+  	-- init
+		config = function ()
+			vim.cmd([[
+			  let g:bullets_set_mappings = 0 " disable adding default key mappings, default = 1
+
+				" default = []
+				" N.B. You can set these mappings as-is without using this g:bullets_custom_mappings option but it
+				" will apply in this case for all file types while when using g:bullets_custom_mappings it would
+				" take into account file types filter set in g:bullets_enabled_file_types, and also
+				" g:bullets_enable_in_empty_buffers option.
+				let g:bullets_custom_mappings = [
+  				\ ['imap', '<cr>', '<Plug>(bullets-newline)'],
+  				\ ['inoremap', '<C-cr>', '<cr>'],
+  				\
+  				\ ['nmap', 'O', 'k<Plug>(bullets-newline)'],
+  				\ ['nmap', 'o', '<Plug>(bullets-newline)'],
+  				\
+  				\ ['vmap', 'gN', '<Plug>(bullets-renumber)'],
+  				\ ['nmap', 'gN', '<Plug>(bullets-renumber)'],
+  				\
+  				\ ['nmap', '<c-space>', '<Plug>(bullets-toggle-checkbox)'],
+  				\
+  				\ ['imap', '<C-t>', '<Plug>(bullets-demote)'],
+  				\ ['nmap', '>>', '<Plug>(bullets-demote)'],
+  				\ ['vmap', '>', '<Plug>(bullets-demote)'],
+  				\ ['imap', '<C-d>', '<Plug>(bullets-promote)'],
+  				\ ['nmap', '<<', '<Plug>(bullets-promote)'],
+  				\ ['vmap', '<', '<Plug>(bullets-promote)'],
+  				\ ]
+				]])
+
+		end,
 	},
 }
