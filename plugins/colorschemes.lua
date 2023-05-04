995a6ea08c067708464cb844d9b19e3e8ebbd404
kraxli <kraxli@pm.me>
2023-05-04 21:24:22 +0200

    remove colorscheme

--- plugins/colorschemes.lua
+++ plugins/colorschemes.lua
@@ -1,9 +1,8 @@
 return {
-  {"rebelot/kanagawa.nvim"},  -- prob the best
-	{"olimorris/onedarkpro.nvim"},
-  {"Mofiqul/vscode.nvim"},
-  {"marko-cerovac/material.nvim"},
-  {"Th3Whit3Wolf/Space-Theme"},
+  { "rebelot/kanagawa.nvim" },  -- prob the best
+	{ "olimorris/onedarkpro.nvim" },
+  { "Mofiqul/vscode.nvim" },
+  { "marko-cerovac/material.nvim" },
 	{ "Th3Whit3Wolf/one-nvim" }, -- lazy by nature
 	{ "sonph/onehalf", rtp = "vim" },
 	{ "NLKNguyen/papercolor-theme" },
