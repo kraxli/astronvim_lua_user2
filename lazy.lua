7781d574908e89611490a32bde0ff7a099551dac
kraxli <kraxli@pm.me>
2023-05-04 21:20:03 +0200

    fix lazy for windows

--- lazy.lua
+++ lazy.lua
@@ -1,4 +1,4 @@
-if vim.fn.has('win32') | vim.fn.has('win64') then
+if vim.fn.has('win32') or vim.fn.has('win64') then
   return { diff = { cmd = "terminal_git" }, checker = { enabled = false }, concurrency = 1, wait = true }
 else
   return { diff = { cmd = "terminal_git" }, checker = { enabled = true } }
