db7cfe04ec9ad266042524cb48e9dc6c4a4f77ad
kraxli <kraxli@pm.me>
2023-05-04 20:58:09 +0200

    lazy config for windows

--- lazy.lua
+++ lazy.lua
@@ -1,1 +1,5 @@
-return { diff = { cmd = "terminal_git" }, checker = { enabled = true } }
+if vim.fn.has('win32') | vim.fn.has('win64') then
+  return { diff = { cmd = "terminal_git" }, checker = { enabled = false }, concurrency = 1, wait = true }
+else
+  return { diff = { cmd = "terminal_git" }, checker = { enabled = true } }
+end
