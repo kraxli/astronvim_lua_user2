722cf62c3d68855b055fd81be332c9cec602c065
kraxli <kraxli@pm.me>
2023-09-11 21:59:57 +0200

    nvim-dap-ui layout

--- plugins/base_plugins_ext.lua
+++ plugins/base_plugins_ext.lua
@@ -1,60 +1,70 @@
-return{
-  {
-    "stevearc/aerial.nvim",
-    version='1.1.0'
-  },
-  -- {
-  --   "rcarriga/nvim-dap-ui",
-  --   opts = function(_, opts)
-  --     opts.layouts = { {
-  --       elements = { {
-  --             id = "scopes",
-  --             size = 0.25
-  --           },
-  --           {
-  --             id = "breakpoints",
-  --             size = 0.25
-  --           },
-  --           {
-  --             id = "stacks",
-  --             size = 0.25
-  --           }, {
-  --             id = "watches",
-  --             size = 0.25
-  --           } },
-  --         position = "left",
-  --         size = 40
-  --       }, {
-  --         elements = { {
-  --             id = "repl",
-  --             size = 0.5
-  --           }, {
-  --             id = "console",
-  --             size = 0.5
-  --           } },
-  --         position = "bottom",
-  --         size = 20
-  --       } }
-  --   end,
+return {
+	{
+		"stevearc/aerial.nvim",
+		version = "1.1.0",
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		opts = function(_, opts)
+			opts.layouts = {
+				{
+					elements = {
+						{
+							id = "scopes",
+							size = 0.25,
+						},
+						{
+							id = "breakpoints",
+							size = 0.25,
+						},
+						{
+							id = "stacks",
+							size = 0.25,
+						},
+						{
+							id = "watches",
+							size = 0.25,
+						},
+					},
+					position = "left",
+					size = 40,
+				},
+				{
+					elements = {
+						{
+							id = "repl",
+							size = 0.5,
+						},
+						{
+							id = "console",
+							size = 0.5,
+						},
+					},
+					position = "bottom",
+					size = 15,
+				},
+			}
+		end,
+	},
 
-    -- the first parameter is the plugin specification
-    -- the second is the table of options as set up in Lazy with the `opts` key
-    -- config = function(plugin, opts)
-    --   -- run the core AstroNvim configuration function with the options table
-    --   require("plugins.configs.nvim-dap-ui")(plugin, opts)
-    --
-    --   local dap_ui = require "nvim-dap-ui"
-    --   dap_ui.layouts = { {
-    --     elements = {  {
-    --         id = "stacks",
-    --         size = 0.25
-    --       },
-    --       -- {
-    --       --   id = "watches",
-    --       --   size = 0.25
-    --       -- }
-    --       },
-    --   } }
-    -- end,
-    -- }
+	-- the first parameter is the plugin specification
+	-- the second is the table of options as set up in Lazy with the `opts` key
+	-- config = function(plugin, opts)
+	--   -- run the core AstroNvim configuration function with the options table
+	--   require("plugins.configs.nvim-dap-ui")(plugin, opts)
+	--
+	--   local dap_ui = require "nvim-dap-ui"
+	--   dap_ui.layouts = { {
+	--     elements = {  {
+	--         id = "stacks",
+	--         size = 0.25
+	--       },
+	--       -- {
+	--       --   id = "watches",
+	--       --   size = 0.25
+	--       -- }
+	--       },
+	--   } }
+	-- end,
+	-- }
 }
