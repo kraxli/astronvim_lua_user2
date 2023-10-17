808f6cf05e4fab3ec8ec503ace0f259ba7e9d51f
kraxli <kraxli@pm.me>
2023-10-17 17:44:47 +0200

    headlines: activate for unix only

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -69,6 +69,7 @@ return {
   },
   {
     "lukas-reineke/headlines.nvim",
+    enabled = vim.fn.has('win') ~= 1,
     dependencies = "nvim-treesitter/nvim-treesitter",
     ft = "markdown",
     opts = {},
